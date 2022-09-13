d6872a4ac8209632a3b6896f4b70df7ee96dc273
havelstech <havelstech@gmail.com>
2022-09-13 11:58:02 -0700

    print alphabet except e and q

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+
+/**
+ * main - print aphabet except q and e
+ *
+ * Return: 0 
+ */
+int main(void)
+{
+	char  ch;
+
+	for (ch = 'a'; ch <= 'z'; ++ch)
+	{
+		if (ch != 101)
+		{
+			if (ch != 113)
+			{
+				putchar(ch);
+			}
+		}
+	}
+	putchar('\n');
+	return (0);
+}
+

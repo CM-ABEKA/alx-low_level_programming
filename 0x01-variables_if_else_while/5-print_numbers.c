03ab44cdfd371d66bcba5a81e5d31ae4806f4525
havelstech <havelstech@gmail.com>
2022-09-13 12:11:01 -0700

    print numbers

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+/**
+ * main - prigram prints single digit numbers
+ *
+ * Return: 0 (Success)
+ */
+int main(void)
+{
+	int n;
+
+	for (n = 0; n < 10; n++)
+	{
+		printf("%i\n", n);
+	}
+	return (0);
+}

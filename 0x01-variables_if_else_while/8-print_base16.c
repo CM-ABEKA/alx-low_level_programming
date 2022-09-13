808479358edb75e5c93dd8abe7f43c4095b1379d
havelstech <havelstech@gmail.com>
2022-09-13 12:36:40 -0700

    hex numbers

---
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+
+/**
+* main - print base16 in lowercase
+*
+* Return: 0 (Success)
+*/
+int main(void)
+{
+	int i;
+
+	for (i = 0; i < 10; i++)
+	{
+		putchar(i % 10 + '0');
+	}
+	for (i = 'a'; i < 'f'; i++)
+	{
+		putchar(i);
+	}
+	putchar('\n');
+	return (0);
+}
+

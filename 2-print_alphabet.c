6fc1f34fcd4c957d974fa2c1584864049d316366
havelstech <havelstech@gmail.com>
2022-09-13 05:57:31 -0700

    print letters of alphabet initial commit

---
+++ 2-print_alphabet.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+/**
+ * main - prints letters of the aphabet
+ *
+ * Description: using putchar
+ * Return: 0 (Success)
+ */
+int main(void)
+{
+	char ch;
+
+	for (ch = 'a'; ch < 'z'; ch++)
+	{
+		putchar(ch);
+	}
+	putchar("\n");
+	return 0;
+}

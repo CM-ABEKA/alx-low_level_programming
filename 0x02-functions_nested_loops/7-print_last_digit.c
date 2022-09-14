e3bc1ed412bfbcbd3ce6171b887abd7a7c5e1b2a
havelstech <havelstech@gmail.com>
2022-09-14 13:26:58 -0700

    add putchar

--- 0x02-functions_nested_loops/7-print_last_digit.c
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -8,5 +8,12 @@
 int print_last_digit(int x)
 {
 	int last_digit = x % 10;
+	if (last_digit < 0)
+	{
+		int out = last_digit * -1;
+		_putchar(out % 10 + '0');
+		return (out);
+	}
+	_putchar(last_digit % 10 + '0');
 	return (last_digit);
 }

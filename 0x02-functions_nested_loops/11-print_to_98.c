42678c5c6599bcb3fd117670826f2026a2ab542e
havelstech <havelstech@gmail.com>
2022-09-14 14:59:30 -0700

    changed whole code.

--- 0x02-functions_nested_loops/11-print_to_98.c
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -1,76 +1,40 @@
 #include "main.h"
 
 /**
- * print_to_98 - print all natural numbers from n to 98
- *@n: starting point
- *
- * Return: 0
+ * print_to_98 - a function that prints all natural numbers from n to 98
+ * user input's number
+ * @n: number input
+ * Return: Always 0 (Success)
  */
 void print_to_98(int n)
 {
-	if (n == 98)
+	if (n < 98)
 	{
-		_putchar(n / 10 + '0');
-		_putchar(n % 10 + '0');
-	}
-	if (n > 98)
-	{
-		for (; n >= 98; n--)
+		while (n <= 98)
 		{
-			_putchar(n / 10 + '0');
-			_putchar(n % 10 + '0');
+			printf("%d", n);
 			if (n != 98)
 			{
-				_putchar(',');
-				_putchar(' ');
+				printf(", ");
 			}
+			n++;
 		}
 	}
-	if (n < 98)
-		for (; n <= 98; n++)
+	else if (n > 98)
+	{
+		while (n >= 98)
 		{
-			if (n > -10 && n < 10)
+			printf("%d", n);
+			if (n != 98)
 			{
-				if (n >= 0)
-				{
-					_putchar(n % 10 + '0');
-					_putchar(',');
-					_putchar(' ');
-				}
-				if (n < 0)
-				{
-					int ng = n * -1;
-
-					_putchar('-');
-					_putchar(ng % 10 + '0');
-					_putchar(',');
-					_putchar(' ');
-				}
-			} else
-			{
-				if (n > 9)
-				{
-					_putchar(n / 10 + '0');
-					_putchar(n % 10 + '0');
-					if (n != 98)
-					{
-						_putchar(',');
-						_putchar(' ');
-					}
-				}
-				if (n < -9)
-				{
-					int lng = n * -1;
-
-					_putchar('-');
-					_putchar(lng / 10 + '0');
-					_putchar(lng % 10 + '0');
-					_putchar(',');
-					_putchar(' ');
-				}
+				printf(", ");
 			}
+			n--;
 		}
-
-	_putchar('\n');
+	}
+	else
+	{
+		printf("98");
+	}
+	printf("\n");
 }
-

a8f2714fb6196c57ee23f27527816ee14a9ca6ce
havelstech <havelstech@gmail.com>
2022-09-14 14:04:22 -0700

    mul table

---
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -0,0 +1,28 @@
+#include "main.h"
+
+/**
+ * times_table - prints the 9 times table
+ *
+ * Return: 0
+ */
+void times_table(void)
+{
+	int row;
+	int col;
+	int mul;
+
+	for (row = 0; row < 10; row++)
+	{
+		for (col = 0; col < 10; col++)
+		{
+			mul = row * col;
+			_putchar(mul);
+			if (col < 9)
+			{
+				_putchar(',');
+				_putchar(' ');
+			}
+		}
+		_putchar('\n');
+	}
+}

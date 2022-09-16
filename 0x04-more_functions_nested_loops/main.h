c04572384b0c9ebbfda1bfa6d6375f33b59a12be
havelstech <havelstech@gmail.com>
2022-09-16 12:42:17 -0700

    errors

--- 0x04-more_functions_nested_loops/main.h
+++ 0x04-more_functions_nested_loops/main.h
@@ -7,6 +7,17 @@
  * void prototype(void)
  */
 
-int _isupper(int);
+int _isupper(int b);
+int _isdigit(int c);
+int mul(int a, int b);
+void print_numbers(void);
+void print_most_numbers(void);
+void more_numbers(void);
+void print_line(int n);
+void print_diagonal(int n);
+void print_square(int size);
+void print_triangle(int size);
+void print_number(int n);
+int _putchar(char c);
 
-#endif
+#endif /* MAIN_H */

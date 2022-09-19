ff5fe32704c1c469ff15a4f541a906284fcc03f8
havelstech <havelstech@gmail.com>
2022-09-19 12:36:37 -0700

    check string length

---
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,25 @@
+#include "main.h"
+
+/**
+ * _strlen - returns the lentgh of a string
+ * @s: string
+ * Return: 0 success
+ */
+int _strlen(char *s)
+{
+	int len = 0;
+	int i;
+
+	for (i = 0;;i++)
+	{
+		if (s[i])
+		{
+			len++;
+		}
+		else if (!s[i])
+		{
+			break;
+		}
+	}
+	return (len);	
+}

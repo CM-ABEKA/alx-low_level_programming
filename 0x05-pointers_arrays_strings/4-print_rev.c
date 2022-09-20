d3606cb5e72a9599a1f432ce052a5331dbc92cc0
havelstech <havelstech@gmail.com>
2022-09-20 12:07:48 -0700

    modified to use midpoint rearrangement

--- 0x05-pointers_arrays_strings/4-print_rev.c
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -11,15 +11,21 @@ void print_rev(char *ch)
 {
 	int len = 0;
 	int i, n;
+	char temp;
 
 	for (i = 0; ch[i] != '\0'; i++)
 	{
 		len ++;
 	}
 
-	for (i = (len - 1); n >= 0; n--)
+	for (n = 0; n < len/2 ; i++)
 	{
-		_putchar(s[i]);
+		temp = ch[i];
+		if (ch[len - i])
+		{
+			ch[i] = ch[len -i];
+		}
+		ch[len - i] = temp;
+		ch[len + 1] = '\0';
 	}
-	_putchar('\n');
 }

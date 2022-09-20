d94266d8b755209d4335ce9ca6d1406e8b40ac55
havelstech <havelstech@gmail.com>
2022-09-20 11:30:46 -0700

    use a while loop to reverse

--- 0x05-pointers_arrays_strings/4-print_rev.c
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -1,4 +1,5 @@
 #include "main.h"
+#include <string.h>
 
 /**
  * print_rev - prints strings in reverse
@@ -6,38 +7,11 @@
  * Return: void
  */
 
-int _strln(char *ch);
 void print_rev(char *ch)
 {
-	int i;
-	int len = _strln(ch)-1;
-	char temp;
-
-	for (i = 0; i < len/2 ; i++)
-	{
-		temp = ch[i];
-		if (ch[len - i - 1])
-		{
-			ch[i] = ch[len - i - 1];
-		}
-		ch[len - i - 1] = temp;
-		ch[len] = '\0';
-	}
-}
-int _strln(char *ch)
-{
-	int i;
-	int len;
+	int len = strlen(ch);
 	
-	len = 0;
-
-	for (i = 0; ; i++)
-	{
-		len++;
-		if (!ch[i])
-		{
-			break;
-		}
-	}
-	return len;
+	while (len--)
+		putchar(*(s + len));
+	putchar('\0');
 }

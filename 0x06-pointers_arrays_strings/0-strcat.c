efc2cdda1f4e0062c383307c94fd241eba6cd1eb
havelstech <havelstech@gmail.com>
2022-09-23 06:42:19 -0700

    without std libraries

--- 0x06-pointers_arrays_strings/0-strcat.c
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -11,12 +11,27 @@ int _strln(char *str);
 char *_strcat(char *dest, char *src)
 {
 	int i;
-	int ln_d = strlen(dest);
-	int ln_s = strlen(src);
+	int ln_d = _strln(dest);
+	int ln_s = _strln(src);
 
 	for (i = 0; i <= ln_s; i++)
 	{
 		dest[ln_d + i] = src[i];
 	}
 	return (dest);
+}
+
+/**
+ * _strln - returns the length of a string
+ * @str: the string
+ * Return: string length
+ */
+int _strln(char *str)
+{
+	int len = 0;
+	int i;
+
+	for (i = 0; str[i] != '\0'; i++)
+		len++;
+	return (len);
 }

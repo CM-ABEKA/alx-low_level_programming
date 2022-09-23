e0eaf78a91fefd3ef708608a0c0fbc605e772539
havelstech <havelstech@gmail.com>
2022-09-23 22:08:20 +0300

    capitalize each letter in a sentence: fixed ouptup error

--- 0x06-pointers_arrays_strings/6-cap_string.c
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -8,23 +8,22 @@
 char *cap_string(char *a)
 {
 	int i, len;
-
 	len = 0;
 	while (a[len])
 		len++;
-
 	for (i = 0; i < len; i++)
 	{
-		if (a[i] >= 100 && a[i] <= 122)
+		if (a[i] >= 97 && a[i] <= 122)
 		{
-			if ((a[i - 1] < 97 && a[i - 1] > 90) || (a[i - 1] > 122))
+			if ((a[i-1] < 97 && a[i - 1] > 90) || a[i - 1] < 65)
 			{
 				a[i] -= 32;
 			}
-			if (a[i - 1] < 65)
+			if (a[i-1] > 122)
+			{
 				a[i] -= 32;
+			}
 		}
 	}
-
 	return (a);
 }

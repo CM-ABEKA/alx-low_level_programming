5678aa74dbf05cc3efc89be5654168eb143e95c4
havelstech <havelstech@gmail.com>
2022-09-23 21:02:53 +0300

    lower to UPPER case in a string

---
+++ 0x06-pointers_arrays_strings/5-string_toupper.c
@@ -0,0 +1,25 @@
+#include "main.h"
+
+/**
+ * string_touper - changes all lowercase to uppercase
+ * @: string
+ * Return: string in caps
+ */
+char *string_toupper(char *s)
+{
+	int diff, low = 97, upp = 65, i, len;
+
+	len = 0;
+	while(s[len])
+		len++;
+	for (i = 0; i < len; i++)
+	{
+		if (s[i] >= low && s[i] <= 122)
+		{
+			diff = s[i] - low;
+			s[i] = upp + diff;
+		}
+	}
+	return (s);
+}
+

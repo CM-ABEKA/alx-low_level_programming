90db050c35d2d1b6ecd4c0498ce663275e9e6ebc
havelstech <havelstech@gmail.com>
2022-09-23 23:56:12 +0300

    leet code

---
+++ 0x06-pointers_arrays_strings/7-leet.c
@@ -0,0 +1,25 @@
+#include "main.h"
+
+/**
+ * leet - encodes string into 1337
+ * @s: the string
+ * Return: char encoded
+ */
+char *leet(char *s)
+{
+	char a[5] = {'A', 'E', 'O', 'T', 'L'};
+	char b[5] = {'4', '3', '0', '7', '1'};
+	int i = 0, k, len = 5;
+	for (k = 0; s[k] != '\0'; k++)
+	{
+		i = 0;
+		for ( ; i < len; i++)
+		{
+			if (s[k] == a[i] || s[k] - 32 == a[i])
+			{
+				s[k] = b[i];
+			}
+		}
+	}
+	return (s);
+}

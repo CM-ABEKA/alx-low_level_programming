b848ea009a1027e7c09daaf619e4f0fa5a328fef
havelstech <havelstech@gmail.com>
2022-09-27 22:22:22 +0300

    function that fills n bytes of memory area pointed to with a constant byte b

---
+++ 0x07-pointers_arrays_strings/main.h
@@ -0,0 +1,11 @@
+#ifndef MAIN_H
+#define MAIN_H
+
+/**
+ * main - header file for prototypes
+ * int prototype(int)
+d * void prototype(void)
+ */
+
+char *_memset(char *s, char b, unsigned int n);
+#endif /* MAIN_H */

422f537f46b8046dfb14356c6d7a8aaaa8ccd1e2
c-abeka <havelstech@gmail.com>
2022-10-05 22:25:03 +0300

    duplicate memory and its contents to a new location

---
+++ 0x0B-malloc_free/1-strdup.c
@@ -0,0 +1,27 @@
+#include "main.h"
+
+/**
+ * _strdup - returns pointer to newly allocated space
+ * @str: the string in storage
+ * Return: a character
+ */
+char *_strdup(char *str)
+{
+	char *p;
+	int i, e;
+
+	if (str == NULL)
+		return (NULL);
+
+	for (e = 0; e <= *str; e++)
+	{
+	}
+	e += 1;
+	p = malloc(sizeof(char) * e);
+
+	for (i = 0; i < e; i++)
+		p[i] = str[i];
+	if (p == NULL)
+		return (NULL);
+	return (p);
+}

bf3d65de16a6dee6500ebcbae47e29ad5234d802
c-abeka <havelstech@gmail.com>
2022-10-05 22:41:55 +0300

    added the files for the project

---
+++ 0x0B-malloc_free/2-str_concat.c
@@ -0,0 +1,9 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "holberton.h"
+/**
+ * str_concat - get ends of input and add together for size
+ * @s1: input one to concat
+ * @s2: input two to concat
+ * Return: concat of s1 and s2
+ */

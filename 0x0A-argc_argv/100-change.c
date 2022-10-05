3f32773780b6e63f362b097aa149c2c9c33b8508
c-abeka <havelstech@gmail.com>
2022-10-05 22:16:06 +0300

    edited slants for betty pass

--- 0x0A-argc_argv/100-change.c
+++ 0x0A-argc_argv/100-change.c
@@ -1,67 +1,49 @@
 #include <stdio.h>
 #include <stdlib.h>
-#include "main.h"
 
 /**
- * main - Entry point
- * @argc: count
- * @argv: cli argiments array
- * Return: (0) success;
- */
-int coins(int amount);
+*main - prints the minimum number of coins to make change
+* for an amount of money
+*@argc: number of arguments
+*@argv: array of arguments
+*Return: returns 1 if there is an error; else returns 0
+*/
+
 int main(int argc, char *argv[])
 {
-	__attribute__ ((unused)) int x = atoi(argv[1]);
-	
+	int cents, coins = 0;
+
 	if (argc != 2)
 	{
-		printf("%s\n", "Error");
+		printf("Error\n");
 		return (1);
 	}
-
-	if (argc < 0)
-		return (0);
-	printf("%d\n", coins(atoi(argv[1])));
-	return (0);
-}
-
-/**
- * coins - get coins for change
- * @amount: amount of money
- * Return: change
- */
-int coins(int amount)
-{
-	int e = 1, f = 2, g = 5, h = 10, i = 25;
-	int bal = 0;
-
-	while (amount > 0)
+	cents = atoi(argv[1]);
+	while (cents > 0)
 	{
-		while (amount >= i)
+		coins++;
+		if ((cents - 25) >= 0)
 		{
-			amount -= i;
-			bal++;
+			cents -= 25;
+			continue;
 		}
-		while (amount >= h)
+		if ((cents - 10) >= 0)
 		{
-			amount -= h;
-			bal++;
+			cents -= 10;
+			continue;
 		}
-		while (amount >= g)
+		if ((cents - 5) >= 0)
 		{
-			amount -= g;
-			bal++;
+			cents -= 5;
+			continue;
 		}
-		while (amount >= f)
+		if ((cents - 2) >= 0)
 		{
-			amount -= f;
-			bal++;
+			cents -= 2;
+			continue;
 		}
-		while (amount >= e)
-		{
-			amount -= f;
-			bal++;
-		}
+		cents--;
 	}
-	return (bal);
+	printf("%d\n", coins);
+	return (0);
 }

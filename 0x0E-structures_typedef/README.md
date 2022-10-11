66f83c39c759afede9d848dde3904846c9f9352d
c-abeka <havelstech@gmail.com>
2022-10-11 19:51:17 +0300

    constructed a struct for a dog

---
+++ 0x0E-structures_typedef/README.md
@@ -0,0 +1,4 @@
+This section entails the following:
+General
+What are structures, when, why and how to use them
+How to use typedef
---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,19 @@
+#ifndef DOG_H
+#define DOG_H
+
+/**
+ * struct dog - this structure contains the details of a dog object
+ * @name: This is the name of the dog
+ * @age: This is the age of the dog
+ * @owner: The owner of the dog
+ *
+ * Description: This is a stuct of a dog
+ */
+struct dog
+{
+	char *name;
+	float age;
+	char *owner;
+};
+
+#endif /* DOG_H */

cdbaf0aecc5e77df286c19705ec5e0601b673684
c-abeka <havelstech@gmail.com>
2022-10-11 22:31:34 +0300

    free dog struct

---
+++ 0x0E-structures_typedef/5-free_dog.c
@@ -0,0 +1,14 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include "dog.h"
+
+/** free_dog - Entry point
+ * @d: struct to be freed
+ * Return: 0;
+ */
+void free_dog(dog_t *d)
+{
+	if (d == NULL)
+		return;
+	free(d);
+}

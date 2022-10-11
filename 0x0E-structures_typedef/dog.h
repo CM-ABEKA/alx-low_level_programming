b3eeb535b4d32e9de87c85aa390ed9a6fbf0fa41
c-abeka <havelstech@gmail.com>
2022-10-11 21:10:30 +0300

    function that prints struct elements

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -9,12 +9,16 @@
  *
  * Description: This is a stuct of a dog
  */
-struct dog
+typedef struct dog
 {
 	char *name;
 	float age;
 	char *owner;
-};
+} dog_t;
 
 void init_dog(struct dog *d, char *name, float age, char *owner);
+void print_dog(struct dog *d);
+dog_t *new_dog(char *name, float age, char *owner);
+void free_dog(dog_t *d);
+
 #endif /* DOG_H */

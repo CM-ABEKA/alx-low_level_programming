9fe5e42628efbe9556cf8d32b0cf3b0436ec3166
c-abeka <havelstech@gmail.com>
2022-10-11 22:43:49 +0300

    repair output for print struct

--- 0x0E-structures_typedef/2-print_dog.c
+++ 0x0E-structures_typedef/2-print_dog.c
@@ -17,11 +17,11 @@ void print_dog(struct dog *d)
 	else
 		printf("Name: %s\n", d->name);
 	if (d->age <= 0)
-		printf("Age: (nil)\n");
+		printf("(nil)\n");
 	else
 		printf("Age: %.6f\n", d->age);
 	if (d->owner == NULL)
-		printf("Owner: (nil)\n");
+		printf("(nil)\n");
 	else
 		printf("Owner: %s\n", d->owner);
 }

3473ed7ab12e286968f22cd17eeac1d4e742a13e
c-abeka <havelstech@gmail.com>
2022-10-15 11:32:33 +0300

    created function that prints a name using function pointers

---
+++ 0x0F-function_pointers/0-print_name.c
@@ -0,0 +1,30 @@
+#include <stdio.h>
+
+/**
+ * f - entry point
+ * @s: string
+ * Return: void
+ */
+void f(char *s)
+{
+	int i, len = 0;
+
+	while (s[len])
+		len++;
+	for (i = 0; i < len; i++)
+	{
+		putchar(s[i]);
+	}
+	putchar('\n');
+}
+
+/**
+ * print_name - entry point
+ * @name: the string
+ * @f: function pointer
+ * Return: none
+ */
+void print_name(char *name, void (*f)(char *))
+{
+	f(name);
+}
---
+++ 0x0F-function_pointers/function_pointers.h
@@ -0,0 +1,7 @@
+#ifndef F_POINTERS_H
+#define F_POINTERS_H
+
+
+void print_name(char *name, void(*f)(char *));
+
+#endif /* F_POINTERS_H */

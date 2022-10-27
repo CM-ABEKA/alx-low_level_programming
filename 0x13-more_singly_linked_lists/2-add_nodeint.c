c8237b66749c6c3cd24bcf12c3347bcd7b11f587
c-abeka <havelstech@gmail.com>
2022-10-27 21:50:28 +0300

    fixed betty for add node at beginning

--- 0x13-more_singly_linked_lists/2-add_nodeint.c
+++ 0x13-more_singly_linked_lists/2-add_nodeint.c
@@ -8,20 +8,18 @@
  */
 listint_t *add_nodeint(listint_t **head, const int n)
 {
-  listint_t *new;
+	listint_t *new;
 
-  if (head == NULL)
-	  return (NULL);
+	if (head == NULL)
+		return (NULL);
+	new = malloc(sizeof(listint_t));
+	if (new == NULL)
+		return (NULL);
 
-  new = malloc(sizeof(listint_t));
-  if (new == NULL)
-    return (NULL);
+	new->n = n;
+	new->next = *head;
 
-  new->n = n;
-  new->next = *head;
+	*head = new;
 
-  *head = new;
-
-  return (new);
+	return (new);
 }
-

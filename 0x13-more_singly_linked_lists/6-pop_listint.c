2955951c2ec760491965332ce43a2714652be04c
c-abeka <havelstech@gmail.com>
2022-10-29 01:38:20 +0300

    remove head node

---
+++ 0x13-more_singly_linked_lists/6-pop_listint.c
@@ -0,0 +1,23 @@
+#include "lists.h"
+
+/**
+ * pop_listint - deletes the head node
+ * @head: head of list
+ * Return: deleted node 0 if empty
+ */
+int pop_listint(listint_t **head)
+{
+	listint_t *temp;
+	int n;
+
+	if (*head == NULL)
+		return (0);
+
+	temp = *head;
+	n = (*head)->n;
+	*head = (*head)->next;
+
+	free(temp);
+
+	return (n);
+}

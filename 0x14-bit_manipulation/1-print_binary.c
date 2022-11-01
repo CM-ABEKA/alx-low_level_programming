b7df035f07abfdcb06a001781d617d1e2b113a48
c-abeka <havelstech@gmail.com>
2022-11-01 21:33:42 +0300

    print binary

--- 0x14-bit_manipulation/1-print_binary.c
+++ 0x14-bit_manipulation/1-print_binary.c
@@ -7,27 +7,10 @@
  */
 void print_binary(unsigned long int n)
 {
-	int i = 0, m = 0, count, temp;
-
-	if (n == 0)
-	{
+	if (n > 1)
+		print_binary(n >> 1);
+	if (n & 1)
+		_putchar('1');
+	else
 		_putchar('0');
-		return;
-	}
-
-	temp = n;
-
-	while (temp != 0)
-	{
-		i++;
-		temp = temp >> 1;
-	}
-	for (count = i - 1; count >= 0; count--)
-	{
-		m = n >> count;
-		if (m & 1)
-			_putchar('1');
-		else
-			_putchar('0');
-	}
 }

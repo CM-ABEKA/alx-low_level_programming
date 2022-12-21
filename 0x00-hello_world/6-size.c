8dbb7bcc85788ccb2b24baf3c17585d2335278f1
c-abeka <havelstech@gmail.com>
2022-12-21 14:13:18 +0300

    task 6 size of types

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -3,7 +3,8 @@
 /**
  * main- Entry point
  *
- * Description: 'this program prints the sizes of the various data types on the host machine'
+ * Description: 'this program prints the sizes of
+ * the various data types on the host machine'
  *
  * Return: 0 (Success)
  */
@@ -12,14 +13,14 @@ int main(void)
 {
 	char ch;
 	int in;
-      	long int lo;
-      	long long int llo;
-      	float fl;
-      
+	long int lo;
+	long long int llo;
+	float fl;
+
 	printf("Size of a char: %i byte(s)\n", sizeof(ch));
-      	printf("Size of an int: %i byte(s)\n", sizeof(in));
-      	printf("Size of a lont int: %i byte(s)\n", sizeof(lo));
-      	printf("Size of a long long int: %i byte(s)\n", sizeof(llo));
-      	printf("Size of a float: %i byte(s)\n", sizeof(fl));
-      	return (0);
+	printf("Size of an int: %i byte(s)\n", sizeof(in));
+	printf("Size of a lont int: %i byte(s)\n", sizeof(lo));
+	printf("Size of a long long int: %i byte(s)\n", sizeof(llo));
+	printf("Size of a float: %i byte(s)\n", sizeof(fl));
+	return (0);
 }

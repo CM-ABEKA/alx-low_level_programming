89a657a7424f9876093cbafd7366c73c615fea20
c-abeka <havelstech@gmail.com>
2023-01-14 12:15:34 +0300

    hash table array fill NULL

--- 0x1A-hash_tables/0-hash_table_create.c
+++ 0x1A-hash_tables/0-hash_table_create.c
@@ -3,25 +3,6 @@
 #include "hash_tables.h"
 
 /**
- * hash_node_create - returns a hash table node
- * @key: the key
- * @value: the value
- * Return: The new node
- */
-hash_node_t *hash_node_create(char *key, char *value)
-{
-        /* Creates a pointer to a new hash table node */
-        hash_node_t *node = (hash_node_t *) malloc(sizeof(hash_node_t));
-        node->key = (char *) malloc(strlen(key) + 1);
-        node->value = (char *) malloc(strlen(value) + 2);
-
-        strcpy(node->key, key);
-        strcpy(node->value, value);
-
-        return (node);
-}
-
-/**
  * hash_table_create - creates a hash table
  * @size: size of table
  *
@@ -32,7 +13,7 @@ hash_table_t *hash_table_create(unsigned long int size)
 	/* creates a pointer to a new hash table*/
 	hash_table_t *table;
 	unsigned long int i;
-
+	
 	table = (hash_table_t *) malloc(sizeof(hash_table_t));
 	if (table == NULL)
 		return (NULL);
@@ -44,7 +25,7 @@ hash_table_t *hash_table_create(unsigned long int size)
 		return (NULL);
 
 	for (i = 0; i < size; i++)
-		table->array = NULL;
+		table->array[i] = NULL;
 
 	return (table);
 }

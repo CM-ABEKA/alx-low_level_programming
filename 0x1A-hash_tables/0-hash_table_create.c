473f107fb01019fe7c4e514283b0a2ac24a97b51
c-abeka <havelstech@gmail.com>
2023-01-14 12:16:40 +0300

    hash table betty check

--- 0x1A-hash_tables/0-hash_table_create.c
+++ 0x1A-hash_tables/0-hash_table_create.c
@@ -13,7 +13,7 @@ hash_table_t *hash_table_create(unsigned long int size)
 	/* creates a pointer to a new hash table*/
 	hash_table_t *table;
 	unsigned long int i;
-	
+
 	table = (hash_table_t *) malloc(sizeof(hash_table_t));
 	if (table == NULL)
 		return (NULL);

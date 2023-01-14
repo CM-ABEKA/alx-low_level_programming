00774984d20676945689f006ac8d01ef0993ce2b
c-abeka <havelstech@gmail.com>
2023-01-14 11:55:53 +0300

    hash table

--- 0x1A-hash_tables/0-hash_table_create.c
+++ 0x1A-hash_tables/0-hash_table_create.c
@@ -8,10 +8,11 @@
  */
 hash_node_t *hash_node_create(char *key, char *value)
 {
-	// Creates a pointer to a new hash table node
-	hash_node_t *node = (hash_node_t*) malloc(sizeof(hash_node_t));
-	node->key = (char*) malloc(strlen(key) + 1);
-	node->value = (char*) malloc(strlen(value) +2);
+	/* Creates a pointer to a new hash table node */
+	hash_node_t *node = (hash_node_t *) malloc(sizeof(hash_node_t));
+
+	node->key = (char *) malloc(strlen(key) + 1);
+	node->value = (char *) malloc(strlen(value) + 2);
 
 	strcpy(node->key, key);
 	strcpy(node->value, value);
@@ -21,4 +22,27 @@ hash_node_t *hash_node_create(char *key, char *value)
 
 /**
  * hash_table_create - creates a hash table
- *
+ * @size: size of table
+ * Return: hash table
+ */
+hash_table_t *hash_table_create(unsigned long int size)
+{
+	/* creates a pointer to a new hash table*/
+	hash_table_t *table;
+	unsigned long int i;
+
+	table = (hash_table_t *) malloc(sizeof(hash_table_t));
+	if (table == NULL)
+		return (NULL);
+
+	table->size = size;
+	table->array = malloc(sizeof(hash_node_t *) * size);
+
+	if (table->array == NULL)
+		return (NULL);
+
+	for (i = 0; i < size; i++)
+		table->array = NULL;
+
+	return (table);
+}

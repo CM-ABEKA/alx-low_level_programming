47e365fbbe5effc7d46394e86cfa0abf84b4061a
cm-abeka <cmabeka2016@gmail.com>
2023-04-13 20:08:22 +0300

    print number combinations in the form of 00 01, 00 02, ..., 99 99;

---
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -0,0 +1,43 @@
+#include <stdio.h>
+
+/**
+ * main - prints number combinations in form 00 01, 00 02...
+ *
+ * Return: 0 on success
+ */
+int main(void)
+{
+	int i = 0, j = 0, k = 0, l = 0;
+
+	while (i < 10)
+	{
+		j = 0;
+		while (j < 10)
+		{
+			k = 0;
+			while (k < 10)
+			{
+				l = 0;
+				while (l < 10)
+				{
+					putchar(i % 10 + '0');
+					putchar(j % 10 + '0');
+					putchar(' ');
+					putchar(k % 10 + '0');
+					putchar(l % 10 + '0');
+					if (i != 9 || k != 9 || l != 9 || i != 9)
+					{
+						putchar(',');
+						putchar(' ');
+					}
+					l++;
+				}
+				k++;
+			}
+			j++;
+		}
+		i++;
+	}
+	putchar('\n');
+	return (0);
+}

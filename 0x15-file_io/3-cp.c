da69cc360b34cd308ce546ac489ffbc24bfca225
cm-abeka <cmabeka2016@gmail.com>
2023-09-08 21:28:43 +0300

    fixed unexpected output

--- 0x15-file_io/3-cp.c
+++ 0x15-file_io/3-cp.c
@@ -29,41 +29,53 @@ int main(int argc, char **argv)
  */
 void copy_file(const char *source, const char *destination)
 {
-	int src_fd, dest_fd;
-	ssize_t n;
-	char buffer[BUFFER_SIZE];
+int src_fd, dest_fd;
+ssize_t n;
+char buffer[BUFFER_SIZE];
 
-	src_fd = open(source, O_RDONLY);
-	if (src_fd == -1)
-	{
-		dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", source);
-		exit(98);
-	}
-	dest_fd = open(destination, O_CREAT | O_WRONLY | O_TRUNC, 0664);
-	if (dest_fd == -1)
-	{
-		dprintf(STDERR_FILENO, "Error: Can't write to %s\n", destination);
-		close(src_fd);
-		exit(99);
-	}
-	while ((n = read(src_fd, buffer, BUFFER_SIZE)) > 0)
-	{
-		if (write(dest_fd, buffer, n) != n)
-		{
-			dprintf(STDERR_FILENO, "Error: Can't write to %s\n", destination);
-			close(src_fd);
-			close(dest_fd);
-			exit(99);
-		}
-	}
+src_fd = open(source, O_RDONLY);
+if (src_fd == -1)
+{
+dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", source);
+exit(98);
+}
 
-	if (n == -1)
-	{
-		dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", source);
-		close(src_fd);
-		close(dest_fd);
-		exit(98);
-	}
-	close(src_fd);
-	close(dest_fd);
+dest_fd = open(destination, O_CREAT | O_WRONLY | O_TRUNC, 0664);
+if (dest_fd == -1)
+{
+dprintf(STDERR_FILENO, "Error: Can't write to file %s\n", destination);
+close(src_fd);
+exit(99);
+}
+
+while ((n = read(src_fd, buffer, BUFFER_SIZE)) > 0)
+{
+if (write(dest_fd, buffer, n) != n)
+{
+dprintf(STDERR_FILENO, "Error: Can't write to file %s\n", destination);
+close(src_fd);
+close(dest_fd);
+exit(99);
+}
+}
+
+if (n == -1)
+{
+dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", source);
+close(src_fd);
+close(dest_fd);
+exit(98);
+}
+
+if (close(src_fd) == -1)
+{
+dprintf(STDERR_FILENO, "Error: Can't close fd %d\n", src_fd);
+exit(100);
+}
+
+if (close(dest_fd) == -1)
+{
+dprintf(STDERR_FILENO, "Error: Can't close fd %d\n", dest_fd);
+exit(100);
+}
 }
